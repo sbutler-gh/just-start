3d033de68aa773140d184647a8ebf81d401731ac
sbutler-gh <sam@ysli.be>
2021-10-24 18:13:28 +0200

    Configuring netlify branch for netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     "extensions": [".svelte", ...mdsvexConfig.extensions],
