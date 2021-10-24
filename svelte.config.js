4f243ba5c814828adc929217fce6daec463cad94
sbutler-gh <sam@ysli.be>
2021-10-24 17:34:25 +0200

    Added static adapter for Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     "extensions": [".svelte", ...mdsvexConfig.extensions],
 
     kit: {
+        adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
