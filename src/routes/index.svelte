d548886c6542ec81817d2dff087180ffdac7f2e6
sbutler-gh <sam@ysli.be>
2021-10-26 21:13:25 +0200

    Adding learn more links to page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1005,14 +1005,14 @@ function copyEventLink() {
         <img id="location_img" class="hidden" src="https://maps.google.com/maps/api/staticmap?markers=icon%3Ahttps%3A%2F%2Fsecure.meetupstatic.com%2Fs%2Fimg%2F5961591462445047%2Fmup-custom-google-map-pin.png%7C38.894615%2C-77.439980&zoom=17&size=480x300&sensor=false&key=AIzaSyA3fSxhHZNQNfMjJuMFvmULACXrBfFz9jQ&signature=UWDg01O1EMndIJNgK_EkV8q8IJE%3D">
         </div>
 
-        <div class="carousel-section">
+        <div class="carousel-section mb-2">
         <p class="text-xl mt-8 mb-4">What do you want to organize?</p>
         <Carousel perPage={{ 800: 3, 500: 2 }} dots={false}>
             <div class="md:mr-2 slide-content">
                 <div class="rounded bg-gray-200 h-64 px-4 py-2">
                     <h3 class="text-xl font-semibold my-2">Identify local issues, desires, and priorities.</h3>
                     <p class="mb-2">Convene with friends and neighbors in your community, listen and share to understand what each other are seeing and wanting, and start organizing for action.</p>
-                    <a class="underline text-blue-500">Learn more</a>
+                    <a href="https://www.youtube.com/watch?v=uEzKUW95t28" target="_blank" class="underline text-blue-500">Learn more</a>
                 </div>
             </div>
             <div class="md:mr-2 slide-content">
@@ -1020,7 +1020,7 @@ function copyEventLink() {
                    <h3 class="text-xl font-semibold my-2">Climate Role-Play Event</h3>
                     <p class="mb-2">Want a live role-playing game, where you and your peers can take climate action towards the Paris Agreement?</p>
                     <!-- <p>Here's how.</p> -->
-                    <a class="underline text-blue-500">Learn more</a>
+                    <a href="https://www.youtube.com/watch?v=V4U3obbn0fA" target="_blank" class="underline text-blue-500">Learn more</a>
                 </div>
             </div>
             <div class="md:mr-2 slide-content">
@@ -1028,7 +1028,7 @@ function copyEventLink() {
                     <h3 class="text-xl font-semibold my-2">C.A.N.</h3>
                     <p class="mb-2">Citizens Action Network.  Community Action Network.  Cities and Neighborhoods.  Climate Action Network.</p>
                     <p class="mb-2">Whatever CAN means to you, you and your community can make it happen.</p>
-                    <a class="underline text-blue-500">Learn more</a>
+                    <a href="https://www.youtube.com/watch?v=HMHMr_o4y4s" target="_blank" class="underline text-blue-500">Learn more</a>
                 </div>
             </div>
             <!-- <div class="ml-2 slide-content">
@@ -1041,9 +1041,9 @@ function copyEventLink() {
             <div class="md:mr-2 slide-content">
                 <div class="rounded bg-gray-200 h-64 px-4 py-2">
                     <h3 class="text-xl font-semibold my-2">Future Design Walk</h3>
-                    <p class="mb-2">Put yourselves in the minds of the people living here 60 years in the future.</p>
-                    <p class="mb-2">Walk around your community, discuss policies and solutions, and imagine from the perspective and on behalf of the people to come.</p>
-                    <a class="underline text-blue-500">Learn more</a>
+                    <p class="mb-2">Put yourselves in the minds of the people living 60 years in the future.</p>
+                    <p class="mb-2">Walk around your community, discuss policies and solutions, and imagine from the perspective on behalf of the people to come.</p>
+                    <a href="https://www.thealternative.org.uk/dailyalternative/2020/10/25/future-design-japan-time-rebels" target="_blank" class="underline text-blue-500">Learn more</a>
                 </div>
             </div>
             <div class="md:mr-2 slide-content">
@@ -1051,7 +1051,7 @@ function copyEventLink() {
                     <h3 class="text-xl font-semibold my-2">Come Together</h3>
                     <p class="mb-2">Message some friends and neighbors, tell them where and when, and start connecting, learning about each other, and building community together.</p>
                     <!-- <p class="mb-2">Walk around your community, discuss policies and solutions, and imagine from the perspective and on behalf of the people to come.</p> -->
-                    <a class="underline text-blue-500">Learn more</a>
+                    <a href="https://www.youtube.com/watch?v=WdHBWL4LK88" target="_blank" class="underline text-blue-500">Learn more</a>
                 </div>
             </div>
         </Carousel>
