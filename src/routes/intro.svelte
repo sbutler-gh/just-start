b16c9c1841db9cb4a680cfc52c19a76b04c43b44
sbutler-gh <sam@ysli.be>
2021-10-28 13:08:41 +0200

    Adding intro page

---
+++ src/routes/intro.svelte
@@ -0,0 +1,12 @@
+<div style="" class="text-center m-auto md:w-8/12 pt-4">
+
+
+<!-- <div class="text-center bg-black text-white p-8">
+    <h1 class="text-2xl mb-2">Not sure how to take action?</h1>
+    <h1 class="text-2xl mb-2">Just start.</h1>
+</div> -->
+
+
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/639909728?h=bce9097e6c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="finaal v6.mov"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
+</div>
