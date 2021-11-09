023f46de16b8bd3ea673f88e756c6cf30a86fe02
sbutler-gh <sam@ysli.be>
2021-11-09 18:07:54 +0100

    Updated copy

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -2,5 +2,6 @@ export const variables = {
     googleCalendar: import.meta.env.VITE_GOOGLE_CALENDAR_API_KEY,
     openCage: import.meta.env.VITE_OPEN_CAGE_API_KEY,
     airNow: import.meta.env.VITE_AIR_NOW_KEY,
-    ipInfo: import.meta.env.VITE_IP_INFO_KEY
+    ipInfo: import.meta.env.VITE_IP_INFO_KEY,
+    contactEmail: 'sam@sambutler.us'
   };
