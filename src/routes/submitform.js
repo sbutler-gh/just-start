4d90ec28014be9ce70458c1dbf9339fc6c3088a0
sbutler-gh <sam@ysli.be>
2021-11-23 11:35:41 +0100

    Troubleshooting endpoint

--- src/routes/submitform.js
+++ src/routes/submitform.js
@@ -1,4 +1,10 @@
-import supabase from "$lib/db"
+// import supabase from "$lib/db"
+import { variables } from '$lib/variables';
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient( import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY, { fetch: fetch })
 
 export async function post(request) {
 
@@ -8,7 +14,7 @@ const {data, error} = await supabase
 
 if (error) {
 return {
-    status: error.status,
+    status: 500,
     body: error
   }
 }
